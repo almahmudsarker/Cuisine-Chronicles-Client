5411c37a96d57e04915bb3f5cfdcda2b439c7276
almahmudsarker <almahmuds427@gmail.com>
2023-05-02 01:19:31 +0600

    route added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './Home/Home';
+import Blog from "./components/Blog/Blog";
+import Login from "./components/Registration/Login/Login";
+import Register from "./components/Registration/Register/Register";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+    ]
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 // Cuisine Chronicles
 // Indian Cuisine Chronicles
